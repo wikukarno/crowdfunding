f213ceb412ef801c6415721a3c07d2337aebae8c
wikukarno <prasetyagama2@gmail.com>
2024-02-01 12:28:52 +0700

    Init Project register user endpoint

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"backend-crowdfunding/handler"
+	"backend-crowdfunding/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+func main()  {
+	dsn := "root:santri@tcp(127.0.0.1:3306)/go-crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	
+	UserHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users", UserHandler.RegisterUser)
+
+	router.Run()
+
+}
